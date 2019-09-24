e5092816ee93200fc913471cc147dc6ec54dff71
poyolpp <pornkanok.li@ku.th>
2019-09-24 18:50:46 +0700

    first commit

---
+++ src/main/java/th/ku/bookservicejpa/BookService.java
@@ -0,0 +1,51 @@
+package th.ku.bookservicejpa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping({"/book"})
+public class BookService {
+
+    @Autowired
+    private BookRepository repository;
+
+    @GetMapping
+    public List<Book> findAll(){
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Book findById(@PathVariable int id) {
+        return repository.findById(id).get();
+    }
+
+    @PostMapping
+    public Book create(@RequestBody Book book){
+        repository.save(book);
+        return repository.findById(book.getId()).get();
+    }
+    @DeleteMapping(path ={"/{id}"})
+    public Book delete(@PathVariable("id") int id) {
+        Book book = repository.findById(id).get();
+        repository.deleteById(id);
+        return book;
+    }
+    @PutMapping(value="/{id}")
+    public Book update(@PathVariable int id, @RequestBody Book book) {
+        Book record = repository.findById(id).get();
+        record.setName(book.getName());
+        record.setPrice(book.getPrice());
+        repository.save(book);
+        return record;
+
+    }
+
+
+
+
+
+}
+
